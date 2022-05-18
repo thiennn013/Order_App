9b0821e5d0dc598e1adb8dda15812b598721620c
thiennn013 <thientronghuynh01@gmail.com>
2022-05-18 21:02:59 +0700

    first

---
+++ FoodMenuApp/FoodMenuApp/View/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using FoodMenuApp.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FoodMenuApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenOrder(object sender, EventArgs e)
+        {
+            if(OrderView.TranslationX >= 150)
+            {
+                Action<double> callback = input => OrderView.TranslationX = input;
+                OrderView.Animate("anim", callback, 150, 0, 16, 300, Easing.CubicInOut);
+            }
+        }
+
+        private void CloseOrder(object sender, EventArgs e)
+        {
+            if (OrderView.TranslationX == 0)
+            {
+                Action<double> callback = input => OrderView.TranslationX = input;
+                OrderView.Animate("anim", callback, 0, 150, 16, 300, Easing.CubicOut);
+            }
+        }
+
+        private void AddOrder(object sender, EventArgs e)
+        {   
+            var item = (sender as Button).BindingContext as Food;
+
+            if (item.Count == 0)
+            {   
+                vm.AddOrder(item);
+                
+            }
+            item.Count++;
+            vm.Load(item);
+            
+        }
+        private void RemoveOrder(object sender, EventArgs e)
+        {
+            var item = (sender as Button).BindingContext as Food;
+          
+            if (item.Count == 1)
+            {
+                 vm.RemoveOrder(item);
+                
+            }
+            
+            item.Count--;
+            vm.Load(item);
+
+
+        }
+        private void DeleteOrder(object sender, EventArgs e)
+        {
+            var item = (sender as Button).BindingContext as Food; 
+            item.Count=0;
+            vm.RemoveOrder(item);
+           
+           
+
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FoodMenuApp/FoodMenuApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,91 @@
+﻿using FoodMenuApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace FoodMenuApp.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+        public MainViewModel()
+        {  
+            Orders = new ObservableCollection<Food>();
+            Foods = GetFoods();
+        }
+        
+        public int OrderCount => Orders.Sum(x => x.Count);
+        public float TotalPrice => Orders.Sum(x => x.Price* x.Count);
+
+    
+        private ObservableCollection<Food> foods;
+        public ObservableCollection<Food> Foods
+        {
+            get { return foods; }
+            set
+            {
+                foods = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Food> orders;
+        public ObservableCollection<Food> Orders
+        {
+            get { return orders; }
+            set
+            {
+                orders = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(OrderCount));
+                OnPropertyChanged(nameof(TotalPrice));
+            }
+        }
+
+
+        private ObservableCollection<Food> GetFoods()
+        {
+            return new ObservableCollection<Food>
+            {
+                new Food{ Name = "Đây Là Tên Món Ăn 1", Image = "anh2.png", Price = 50000f, Description = "Đây là phần mô tả món ăn, bao gồm cách chế biến và các nguyên liệu của nó, phần này chưa được mô tả !!!"},
+                new Food{ Name = "Đây Là Tên Món Ăn 2", Image = "anh8.png", Price = 100000f, Description = "Đây là phần mô tả món ăn, bao gồm cách chế biến và các nguyên liệu của nó, phần này chưa được mô tả !!!"},
+                new Food{ Name = "Đây Là Tên Món Ăn 3", Image = "anh3.png", Price = 150000f, Description = "Đây là phần mô tả món ăn, bao gồm cách chế biến và các nguyên liệu của nó, phần này chưa được mô tả !!!"},
+                new Food{ Name = "Đây Là Tên Món Ăn 4", Image = "anh9.png", Price = 200000f, Description = "Đây là phần mô tả món ăn, bao gồm cách chế biến và các nguyên liệu của nó, phần này chưa được mô tả !!!"},
+                new Food{ Name = "Đây Là Tên Món Ăn 5", Image = "anh1.png", Price = 300000f, Description = "Đây là phần mô tả món ăn, bao gồm cách chế biến và các nguyên liệu của nó, phần này chưa được mô tả !!!"},
+                new Food{ Name = "Đây Là Tên Món Ăn 6", Image = "anh4.png", Price = 300000f, Description = "Đây là phần mô tả món ăn, bao gồm cách chế biến và các nguyên liệu của nó, phần này chưa được mô tả !!!"},
+               
+            };
+        }
+
+
+        public void AddOrder(Food item)
+        {
+            if (item != null)
+            {
+                Orders.Add(item);
+                OnPropertyChanged(nameof(OrderCount));
+                OnPropertyChanged(nameof(TotalPrice));
+            }
+        }
+        public void RemoveOrder(Food item)
+        {
+            if (item != null)
+            {
+                Orders.Remove(item);
+                OnPropertyChanged(nameof(OrderCount));
+                OnPropertyChanged(nameof(TotalPrice));
+            }
+        }
+
+        public void Load(Food item)
+        {   if(item.Count !=0)
+            Orders[Orders.IndexOf(item)] = item; 
+            OnPropertyChanged(nameof(item));
+            OnPropertyChanged(nameof(OrderCount));
+            OnPropertyChanged(nameof(TotalPrice));
+            
+        }
+    }
+}
